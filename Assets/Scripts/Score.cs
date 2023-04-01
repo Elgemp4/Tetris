5320c3cb77d232155cea56201ab02ce93ed9c874
Elgemp4 <69595917+Elgemp4@users.noreply.github.com>
2023-04-01 10:54:36 +0200

    created diverses classes

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,10 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playfield : MonoBehaviour
+public class Score : MonoBehaviour
 {
-    
-    
     // Start is called before the first frame update
     void Start()
     {
